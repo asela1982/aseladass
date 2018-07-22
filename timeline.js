a9484e4c9983313dd7111eedd2c655c79d42b8d5
Asela Dassanayake <asela.d82@gmail.com>
2018-07-22 20:09:37 +0530

    add new job at frimi senior manager

--- timeline.js
+++ timeline.js
@@ -6,7 +6,7 @@
     {id: 1, content: 'BS in Physical Sciences',start: '2003-08-01', end: '2006-08-01', style:"color:white;background-color: #303B60;border:0px"},
     {id: 2, content: 'Advanced Diploma in Mgt. Accountancy(CIMA)',start: '2003-08-01', end: '2006-08-01', style:"color:white;background-color: #303B60;border:0px"},
     {id: 3, content: 'MBA in Int.Finance', start: '2010-08-01', end: '2012-05-01',style:"color:white;background-color: #303B60;border:0px"},
-    {id: 4, content: 'UT Data Analytics & Visualization', start: '2017-10-03',style:"color:white;background-color: #303B60;border:0px"},
+    {id: 4, content: 'UT Data Analytics & Visualization', start: '2017-10-03', end: '2018-04-18', style:"color:white;background-color: #303B60;border:0px"},
     {id: 5, content: 'Employee of the Year(Nominated)', start: '2014-03-01',style:"color:white;background-color: #2962FF;border:0px"},
     {id: 6, content: 'Manager Data Analytics(M2), Nations Trust Bank', start: '2015-01-01', end: '2017-07-01',style:"color:white;background-color: #20A5C0;border:0px"},
     {id: 7, content: 'Consumer Loans Manager(M1), Nations Trust Bank', start: '2012-05-01', end: '2014-12-31',style:"color:white;background-color: #20A5C0;border:0px"},
@@ -14,7 +14,10 @@
     {id: 9, content: 'Officer, Consumer Credit, Standard Chartered Bank', start: '2008-08-01', end: '2010-07-30',style:"color:white;background-color: #20A5C0;border:0px"},
     {id: 10, content: 'John Molson MBA Case Competition', start: '2011-01-01',style:"color:white;background-color: #303B60;border:0px"},
     {id: 11, content: 'Tableau Conference on Tour', start: '2016-01-01',style:"color:white;background-color: #303B60;border:0px"},
-    {id: 12, content: 'SciPy Conference, Austin, Texas', start: '2017-07-10',style:"color:white;background-color: #303B60;border:0px"}
+    {id: 12, content: 'SciPy Conference, Austin, Texas', start: '2017-07-10',style:"color:white;background-color: #303B60;border:0px"},
+    {id: 13, content: 'Senior Manager-Data Analytics, FriMi Digital Bank', start: '2018-05-02',style:"color:white;background-color: #20A5C0;border:0px"}
+    
+  
   ]);
 
   // Configuration for the Timeline
