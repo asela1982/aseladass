d7bc90fc56c3d947f83a494fa6ca15a8f0a0bee1
Asela Dassanayake <asela.d82@gmail.com>
2018-07-22 20:50:17 +0530

    insert new access token

--- leaflet.js
+++ leaflet.js
@@ -16,5 +16,5 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.emerald',
-    accessToken: "pk.eyJ1IjoiYXNlbGExOTgyIiwiYSI6ImNqZDNocXRlNTBoMWEyeXFmdWY1NnB2MmIifQ.ziEOjgHun64EAp4W3LlsQg"
+    accessToken: "pk.eyJ1IjoiYXNlbGExOTgyIiwiYSI6ImNqaTJ5MWl6MzFzZmcza255emwxMjBnZGMifQ.fEWF7ZWAMNRFFPG6WGbzGg"
 }).addTo(mymap);
